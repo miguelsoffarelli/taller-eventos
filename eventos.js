f77fd462b06423811214b9e9fa866ca994318b73
miguelsoffarelli <137739802+miguelsoffarelli@users.noreply.github.com>
2023-08-11 03:33:57 -0300

    Solución implementada

--- eventos.js
+++ eventos.js
@@ -1,5 +1,12 @@
 const cont = document.getElementById("btn-container");
+const greet = document.getElementById("greet-btn");
 
-cont.addEventListener("click", () => {
+
+cont.addEventListener("click", (event) => {
     alert("Hola! Soy el div.");
+});
+
+greet.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Hola!");
 });
